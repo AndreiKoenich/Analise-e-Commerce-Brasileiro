aa68f2301e63dc419003b23a46c2b36060eac5fa
Andrei <andrei.koenich@gmail.com>
2024-11-21 13:03:37 -0300

    Renomeando diretório

--- eCommerceAnalysis/eCommerceAnalysis/wsgi.py
+++ eCommerceAnalysis/eCommerceAnalysis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerceAnalysis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eCommerceAnalysis.settings')
 
 application = get_wsgi_application()
