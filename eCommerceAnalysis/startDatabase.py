0bccb48e909e2c57bb2a1f33e8611d6694fa73b5
Andrei <andrei.koenich@gmail.com>
2024-11-25 09:17:14 -0300

    Adicionada tabela faltante olist_order_items_dataset.csv

--- eCommerceAnalysis/startDatabase.py
+++ eCommerceAnalysis/startDatabase.py
@@ -28,6 +28,19 @@ def create_tables(engine):
         );
         """,
         """
+        DROP TABLE IF EXISTS order_items_dataset;
+        CREATE TABLE order_items_dataset (
+            order_id VARCHAR(255),
+            order_item_id INT,
+            product_id VARCHAR(255),
+            seller_id VARCHAR(255),
+            shipping_limit_date TIMESTAMP,
+            price FLOAT,                              
+            freight_value FLOAT,
+            PRIMARY KEY (order_id, order_item_id)
+        );
+        """,
+        """
         DROP TABLE IF EXISTS olist_order_payments_dataset;
         CREATE TABLE olist_order_payments_dataset (
             order_id VARCHAR(255),
